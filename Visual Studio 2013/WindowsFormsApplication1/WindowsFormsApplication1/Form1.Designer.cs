1342b79ee803709d1e8bb08b1efa618729fa6768
Thuan Duc Nguyen <vpenguiin@gmail.com>
2015-02-11 21:45:14 +0100

    some updates to converter

--- Visual Studio 2013/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ Visual Studio 2013/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -30,11 +30,14 @@
     {
       this.button1 = new System.Windows.Forms.Button();
       this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+      this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+      this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
       this.SuspendLayout();
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(101, 199);
+      this.button1.Location = new System.Drawing.Point(77, 62);
       this.button1.Name = "button1";
       this.button1.Size = new System.Drawing.Size(75, 23);
       this.button1.TabIndex = 0;
@@ -44,18 +47,56 @@
       // 
       // richTextBox1
       // 
-      this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+      this.richTextBox1.Location = new System.Drawing.Point(258, 102);
       this.richTextBox1.Name = "richTextBox1";
       this.richTextBox1.ReadOnly = true;
-      this.richTextBox1.Size = new System.Drawing.Size(260, 144);
+      this.richTextBox1.Size = new System.Drawing.Size(261, 29);
       this.richTextBox1.TabIndex = 1;
       this.richTextBox1.Text = "Converts your stats from HSv3 to HSv4";
+      this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+      // 
+      // richTextBox2
+      // 
+      this.richTextBox2.Location = new System.Drawing.Point(12, 102);
+      this.richTextBox2.Name = "richTextBox2";
+      this.richTextBox2.Size = new System.Drawing.Size(223, 371);
+      this.richTextBox2.TabIndex = 2;
+      this.richTextBox2.Text = "";
+      this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+      // 
+      // richTextBox3
+      // 
+      this.richTextBox3.Location = new System.Drawing.Point(534, 102);
+      this.richTextBox3.Name = "richTextBox3";
+      this.richTextBox3.Size = new System.Drawing.Size(223, 371);
+      this.richTextBox3.TabIndex = 3;
+      this.richTextBox3.Text = "";
+      this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+      this.tableLayoutPanel1.ColumnCount = 3;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 65F));
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(258, 153);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 1;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.125F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.875F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(261, 320);
+      this.tableLayoutPanel1.TabIndex = 4;
+      this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(284, 262);
+      this.ClientSize = new System.Drawing.Size(796, 494);
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Controls.Add(this.richTextBox3);
+      this.Controls.Add(this.richTextBox2);
       this.Controls.Add(this.richTextBox1);
       this.Controls.Add(this.button1);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -70,6 +111,9 @@
 
     private System.Windows.Forms.Button button1;
     private System.Windows.Forms.RichTextBox richTextBox1;
+    private System.Windows.Forms.RichTextBox richTextBox2;
+    private System.Windows.Forms.RichTextBox richTextBox3;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
   }
 }
 
--- Visual Studio 2013/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ Visual Studio 2013/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace WindowsFormsApplication1
 {
@@ -19,73 +20,139 @@ namespace WindowsFormsApplication1
 
     private void button_Convert(object sender, EventArgs e)
     {
-      string stats = System.IO.File.ReadAllText("Stat.txt");
-      string[] statsArray = Text.Split(' ');
-      string[] finalArray;
-      List<string> = new List
-      string className = " ";
+      string stats = System.IO.File.ReadAllText(@"D:\Uni\P2\CSharp\Visual Studio 2013\Projects\HS\Stats.txt");
+      string[] statsArray = stats.Split(' ');
+      List<string> l = new List<string>();
+      string className = "";
       int win = 0;
       int loss = 0;
       int deleted = 0;
-      int i = 0, k = 0, j = 0;
+      int i = 0, j = 0, k = 0;
 
       foreach (string s in statsArray)
       {
-        if (i == 0)
+        if (j == 1)
         {
-          switch (k = Convert.ToInt32(s))
+          Int32.TryParse(s, out i);
+          switch (i)
           {
             case 1:
-              className = "Druid";
+              className = "Druid       ";
               break;
             case 2:
-              className = "Hunter";
+              className = "Hunter     ";
               break;
             case 3:
-              className = "Mage";
+              className = "Mage      ";
               break;
             case 4:
-              className = "Paladin";
+              className = "Paladin    ";
               break;
-            case 5: 
-              className = "Priest";
+            case 5:
+              className = "Priest       ";
               break;
             case 6:
-              className = "Rogue";
+              className = "Rogue     ";
               break;
             case 7:
-              className = "Shama";
+              className = "Shaman  ";
               break;
             case 8:
-              className = "Warlock";
+              className = "Warlock  ";
               break;
             case 9:
-              className = "Warrior";
+              className = "Warrior    ";
+              break;
+            default:
+              className = "Failed     ";
               break;
           }
-          i++;
+          j = 2;
         }
-        if (i == 1)
+        else if (j == 2)
         {
-          win = Convert.ToInt32(s);
-          i++;
+          Int32.TryParse(s, out win);
+          j = 3;
         }
-        if (i == 2)
+        else if (j == 3)
         {
-          loss = Convert.ToInt32(s);
-          i++;
+          Int32.TryParse(s, out loss);
+          j = 4;
         }
-        if (i == 3)
+        else if (j == 4)
         {
-          deleted = Convert.ToInt32(s);
-          i = 0;
+          if (Int32.TryParse(s, out deleted))
+          {
+            if (win > 9)
+            {
+              l.Add(className + " with " + win + " wins and  " + loss + " losses ");
+            }
+            else if(win > 1)
+            { 
+              l.Add(className + " with   " + win + " wins and  " + loss + " losses "); 
+            }
+            else
+            {
+              l.Add(className + " with   " + win + " win and  " + loss + " losses ");
+            }
+            j = 1;
+          }
+          else
+          {
+            l.Add("This run is deleted");
+            j = 1;
+          }
         }
-
-        finalArray[j] = className + " " + win + " " + loss + " " + deleted;
-        j++;
+        else
+          j = 1;
       }
 
-      richTextBox1.Text = "All Stats Has Been Converted!!";
+      foreach (string s in l)
+      {
+        richTextBox3.Text += s + "\n";
+      }
+      win = 0;
+      loss = 0;
+      int matches = 0;
+      float winp = 0;
+      foreach (string s in l)
+      {
+        if(s.Contains("Mage"))
+        {
+          string pattern = @"[0-9]+";
+
+          Match m = Regex.Match(s, pattern);
+          win += Convert.ToInt32(m.Value);
+          m = m.NextMatch();
+          loss += Convert.ToInt32(m.Value);
+
+          richTextBox2.Text += s + "\n";
+        }
+      }
+      winp = ((float)win / ((float)win + (float)loss)) * 100;
+      matches = win + loss;
+      richTextBox2.Text += "\nTotal is "+ win + " wins and " + loss + " losses\n";
+      richTextBox2.Text += "\nTotal matches is " + matches + " \n" + "Win percentage of " + winp.ToString("N") + " %\n";
+    }
+
+    private void richTextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void richTextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void richTextBox3_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+    {
+
     }
   }
 }
