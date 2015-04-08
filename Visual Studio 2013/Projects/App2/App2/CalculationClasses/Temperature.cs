fba9cf39a895dcbcf6aef27d6770e364312a9e3e
Thuan Duc Nguyen <vpenguiin@gmail.com>
2015-04-08 11:41:52 +0200

    Tried something with Xamerin

---
+++ Visual Studio 2013/Projects/App2/App2/CalculationClasses/Temperature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2.CalculationClasses
+{
+  class Temperature
+  {
+
+    public static double CelciusToKelvin(double celcius)
+    {
+      return celcius + 273.15;
+    }
+
+  }
+}
