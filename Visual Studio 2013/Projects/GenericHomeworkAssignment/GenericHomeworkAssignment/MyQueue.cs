2c86b79925c30d9ee15d3a9751be8c0f56c1e04a
Thuan Duc Nguyen <vpenguiin@gmail.com>
2015-04-09 21:01:20 +0200

    Generic Homework Assignment Startet

---
+++ Visual Studio 2013/Projects/GenericHomeworkAssignment/GenericHomeworkAssignment/MyQueue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericHomeworkAssignment
+{
+  public class MyQueue<T> where T : IComparable<T>
+  {
+    private List<QueueItem<T>> Queue = new List<QueueItem<T>>();
+
+    public void Enqueue(QueueItem<T> element)
+    {
+      Queue.Add(element);
+    }
+
+    public bool Dequeue(QueueItem<T> priority)
+    {
+      return Queue.Remove(priority);
+    }
+
+
+  }
+}
