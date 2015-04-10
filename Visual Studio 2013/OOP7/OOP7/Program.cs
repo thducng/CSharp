36115d04108d6d16f924cacc312d3ff274d8528d
Thuan Duc Nguyen <vpenguiin@gmail.com>
2015-04-10 13:24:47 +0200

    OOP 7 opgaver

---
+++ Visual Studio 2013/OOP7/OOP7/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP7
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      MyQueue<string> myque = new MyQueue<string>();
+
+      myque.Add("something");
+      myque.Add("Somethingmore");
+      myque.Add("somethingafterthat");
+
+      Console.WriteLine(myque[0]);
+
+      Console.ReadKey();
+    }
+  }
+
+  public class MyQueue<T> where T : ICollection<T>
+  {
+    private List<T> Queue = new List<T>();
+
+    public MyQueue()
+    {
+      size = 0;
+      head = null;
+    }
+
+    public void Add(T t)
+    {
+      Queue.Add(t);
+
+    }
+
+    public bool Remove(T item)
+    {
+      return Queue.Remove(item);
+    }
+
+    public void Clear()
+    {
+      Queue.Clear();
+    }
+
+    public int WordCount()
+    {
+      return Queue.Count;
+    }
+
+
+
+  }
+
+}
